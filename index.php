5743bc6b8c82738c39a1e6ce8330bc2fd41d33f9
Lambert Agbeehia <lambmeister7@gmail.com>
2021-11-19 21:29:36 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,46 +1,123 @@
 <?php
-        #We obtain the data which is contained in the post url on our server.
+// Reads the variables sent via POST
+$sessionId   = $_POST["sessionId"];
+$serviceCode = $_POST["serviceCode"];
+$text = $_POST["text"];
 
-        $text=$_GET['USSD_STRING'];
-        $phonenumber=$_GET['MSISDN'];
-        $serviceCode=$_GET['serviceCode'];
+
+//This is the first menu screen
+
+if ( $text == "" ) {
+  $response  = "CON Welcome to The PASSCO App \n";
+  $response .= "1. Enter 1 to continue";
+}
 
 
-        $level = explode("*", $text);
-        if (isset($text)) {
-   
+// Menu for a user who selects '1' from the first menu
+// Will be brought to this second menu screen
 
-        if ( $text == "" ) {
-            $response="CON Welcome to the registration portal.\nPlease enter you full name";
-        }
+else if ($text == "1") {
+  $response  = "CON  Pick a table for reservation below \n";
+  $response .= "1. Table for 2 \n";
+  $response .= "2. Table for 4 \n";
+  $response .= "3. Table for 6 \n";
+  $response .= "4. Table for 8 \n";
+}
 
-        if(isset($level[0]) && $level[0]!="" && !isset($level[1])){
 
-          $response="CON Hi ".$level[0].", enter your ward name";
-             
-        }
-        else if(isset($level[1]) && $level[1]!="" && !isset($level[2])){
-                $response="CON Please enter you national ID number\n"; 
+//Menu for a user who selects '1' from the second menu above
+// Will be brought to this third menu screen
+else if ($text == "1*1") {
+  $response = "CON You are about to book a table for 2 \n";
+  $response .= "Please Enter 1 to confirm \n";
+}
 
-        }
-        else if(isset($level[2]) && $level[2]!="" && !isset($level[3])){
-            //Save data to database
-            $data=array(
-                'phonenumber'=>$phonenumber,
-                'fullname' =>$level[0],
-                'electoral_ward' => $level[1],
-                'national_id'=>$level[2]
-                );
+else if ($text == "1*1*1") {
+  $response = "CON Table for 2 cost -N- 50,000.00 \n";
+  $response .= "Enter 1 to continue \n";
+  $response .= "Enter 0 to cancel";
+}
 
+else if ($text == "1*1*1*1") {
+  $response = "END Your Table reservation for 2 has been booked";
+}
+
+
+else if ($text == "1*1*1*0") {
+  $response = "END Your Table reservation for 2 has been canceled";
+}
+
+// Menu for a user who selects "2" from the second menu above
+// Will be brought to this fourth menu screen
+else if ($text == "1*2") {
+  $response = "CON You are about to book a table for 4 \n";
+  $response .= "Please Enter 1 to confirm \n";
+}
+
+// Menu for a user who selects "1" from the fourth menu screen
+else if ($text == "1*2*1") {
+  $response = "CON Table for 4 cost -N- 150,000.00 \n";
+  $response .= "Enter 1 to continue \n";
+  $response .= "Enter 0 to cancel";
+}
+        
+else if ($text == "1*2*1*1") {
+  $response = "END Your Table reservation for 4 has been booked";
+}
+    
+else if ($text == "1*2*1*0") {
+  $response = "END Your Table reservation for 4 has been canceled";
+}
+
+// Menu for a user who enters "3" from the second menu above
+// Will be brought to this fifth menu screen
+else if ($text == "1*3") {
+  $response = "CON You are about to book a table for 6 \n";
+  $response .= "Please Enter 1 to confirm \n";
+}
+
+// Menu for a user who enters "1" from the fifth menu
+else if ($text == "1*3*1") {
+  $response = "CON Table for 6 cost -N- 250,000.00 \n";
+  $response .= "Enter 1 to continue \n";
+  $response .= "Enter 0 to cancel";
+}
+
+else if ($text == "1*3*1*1") {
+  $response = "END Your Table reservation for 6 has been booked";
+}
+        
+else if ($text == "1*3*1*0") {
+  $response = "END Your Table reservation for 6 has been canceled";
+}
+
+// Menu for a user who enters "4" from the second menu above
+// Will be brought to this sixth menu screen
+else if ($text == "1*4") {
+  $response = "CON You are about to book a table for 8 \n";
+  $response .= "Please Enter 1 to confirm \n";
+}
+
+// Menu for a user who enters "1" from the sixth menu
+else if ($text == "1*4*1") {
+  $response = "CON Table for 8 cost -N- 250,000.00 \n";
+  $response .= "Enter 1 to continue \n";
+  $response .= "Enter 0 to cancel";
+}
+                       
+else if ($text == "1*4*1*1") {
+  $response = "END Your Table reservation for 8 has been booked";
+}
             
+else if ($text == "1*4*1*0") {
+  $response = "END Your Table reservation for 8 has been canceled";
+}
 
-            $response="END Thank you ".$level[0]." for registering.\nWe will keep you updated"; 
-    }
 
-        header('Content-type: text/plain');
-        echo $response;
+//echo response
 
-    }
+header('Content-type: text/plain');
+echo $response
+
 
 ?>
-
