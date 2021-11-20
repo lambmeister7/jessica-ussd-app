fe19c9f78ecd67b754e11fc882c14b4840c96055
Lambert Agbeehia <lambmeister7@gmail.com>
2021-11-20 09:12:56 +0000

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $text = $_POST["text"];
 //This is the first menu screen
 
 if ( $text == "" ) {
-  $response  = "CON Hi welcome, I can help you with Event Reservation \n";
+  $response  = "CON Welcome to the PASSCO app \n";
   $response .= "1. Enter 1 to continue";
 }
 
@@ -16,18 +16,17 @@ if ( $text == "" ) {
 // Will be brought to this second menu screen
 
 else if ($text == "1") {
-  $response  = "CON  Pick a table for reservation below \n";
-  $response .= "1. Table for 2 \n";
-  $response .= "2. Table for 4 \n";
-  $response .= "3. Table for 6 \n";
-  $response .= "4. Table for 8 \n";
+  $response  = "CON  Select exams type \n";
+  $response .= "1. BECE \n";
+  $response .= "2. WASSCE \n";
+  
 }
 
 
 //Menu for a user who selects '1' from the second menu above
 // Will be brought to this third menu screen
 else if ($text == "1*1") {
-  $response = "CON You are about to book a table for 2 \n";
+  $response = "CON Select Subject \n";
   $response .= "Please Enter 1 to confirm \n";
 }
 
